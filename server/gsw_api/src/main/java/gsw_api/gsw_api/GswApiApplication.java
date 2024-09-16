bc1d1b7ed8f6dd9ddc5b2105ccdeb453b8470bd2
Aline Ramos <amr.aline@gmail.com>
2024-09-16 10:22:40 -0300

    Rota GET noticias ID

--- server/gsw_api/src/main/java/gsw_api/gsw_api/GswApiApplication.java
+++ server/gsw_api/src/main/java/gsw_api/gsw_api/GswApiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -68,4 +69,11 @@ class RestGSW_APIController {
 		noticias.add(noticia);
 		return noticia;
 	}
+	@GetMapping("/noticias/{id}")
+    public Noticia getNoticiaById(@PathVariable String id) {
+        return noticias.stream()
+            .filter(noticia -> noticia.getIdNoticia().equals(id))
+            .findFirst()
+            .orElse(null); // Você pode retornar uma exceção personalizada se preferir
+    }
 }

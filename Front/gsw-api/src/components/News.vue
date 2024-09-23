d6792d57c6bf2198550069c611a802999ea92d1a
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2024-09-23 20:05:15 -0300

    A3F-14 - merge com tasks finalizadas para sprint1 (#3)
    * [A3F-7] Rota GET mockada de Portal de Noticias
    
    * implementacao classe noticia
    
    * Rota GET noticias ID
    
    * Rota GET portal ID
    
    * Update README.md (#1)
    
    * CoderHood home portal
    
    * Added icon and tab name
    
    * ./gitignore incremented
    
    * Created Routes and components, / and Portals
    
    * Layout adjustment
    
    * Created news table skeleton
    
    * Implemented filter method for items list
    
    * A3F-14 - Front consumindo mocks do backend
    
    ---------
    
    Co-authored-by: Alexandre Jonas <jonassouzafonseca@gmail.com>
    Co-authored-by: Aline Ramos <amr.aline@gmail.com>
    Co-authored-by: allineramos <144153142+allineramos@users.noreply.github.com>
    Co-authored-by: Trevizoli <rtrevizoli@yahoo.com.br>
    Co-authored-by: Rennerson13 <rennerson.afonso@fatec.sp.gov.br>

--- Front/gsw-api/src/components/News.vue
+++ Front/gsw-api/src/components/News.vue
@@ -1,5 +1,4 @@
 <template>
-
   <v-container class="d-flex align-center pe-2">
     <h1>Notícias</h1>
     
@@ -19,74 +18,59 @@
     ></v-text-field>
   </v-container>
 
-    <v-container>
-      <v-row v-for="item in filteredItems" :key="item.id">
-        <v-col >
-          <v-card elevation="2" rounded>
-                <v-card-title position-static>
-                  {{ item.title }}
-                </v-card-title>
-                <v-card-text>
-                  {{ item.name }}
-                </v-card-text>
+  <v-container>
+    <v-row v-for="item in filteredItems" :key="item.idNoticia">
+      <v-col>
+        <v-card elevation="2" rounded>
+          <v-card-title position-static>
+            {{ item.titulo }}
+          </v-card-title>
+          <v-card-text>
+            {{ item.autor }}
+          </v-card-text>
         </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
-  export default {
-    data () {
-      return {
-        search: '',
-        items: [
-          {
-            id: 1,
-            title: 'Graphics Card - 1',
-            image: '1.png',
-            name: 'Nebula GTX 3080',
-          },
-          {
-            id: 2,
-            title: 'Graphics Card - 2',
-            image: '2.png',
-            name: 'Galaxy RTX 3080',
-          },
-          {
-            id: 3,
-            title: 'Graphics Card - 3',
-            image: '3.png',
-            name: 'Orion RX 6800 XT',
-          },
-          {
-            id: 4,
-            title: 'Graphics Card - 4',
-            image: '4.png',
-            name: 'Vortex RTX 3090',
-          },
-          {
-            id: 5,
-            title: 'Graphics Card - 5',
-            image: '5.png',
-            name: 'Cosmos GTX 1660 Super',
-          },
-        ],
-        filteredItems: [],
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      search: '',
+      items: [],
+      filteredItems: [],
+    };
+  },
+  mounted() {
+    this.fetchNoticias();
+  },
+  methods: {
+    async fetchNoticias() {
+      try {
+        const response = await axios.get('http://localhost:8080/gsw_api/noticias'); 
+        console.log(response.data); // 
+        this.items = response.data; 
+        this.filteredItems = this.items; 
+      } catch (error) {
+        console.error('Erro ao buscar notícias:', error);
       }
     },
-    mounted() {
-      this.filteredItems = this.items;
-    },
-    methods: {
-      filterItems()
-      {
-        const searchTerm = this.search.toLowerCase();
-        
-        this.filteredItems = this.items.filter(item =>
-          item.title.toLowerCase().includes(searchTerm) ||
-          item.name.toLowerCase().includes(searchTerm)
+    filterItems() {
+      const searchTerm = this.search.toLowerCase();
+      
+      this.filteredItems = this.items.filter(item =>
+        item.titulo.toLowerCase().includes(searchTerm) ||
+        item.autor.toLowerCase().includes(searchTerm)
       );
-      },
     },
-  };
+  },
+};
 </script>
+
+<style>
+/* Estilos opcionais */
+</style>

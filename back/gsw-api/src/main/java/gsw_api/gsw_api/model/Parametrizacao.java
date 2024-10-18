2ebaae594f8b7ac705fb5691a6375c1802fd04ec
Alexandre Jonas <jonassouzafonseca@gmail.com>
2024-10-17 21:57:22 -0300

    A3F-32 [Backend]: Desenvolver serviço para coleta de informações (scraping) de sites de notícias.
    Descrição da Implementação
    - Rotina para puxar todos os portais do banco, e a partir deles gerar notícias através do webscraping, salvando essas notícias no banco
    
    Sugestões de Melhorias Futuras
    - Validar/FIltrar notícias que vêm com todos os campos vazios
    - Validar se a notícia sendo inserida já existe no banco

--- back/gsw-api/src/main/java/gsw_api/gsw_api/model/Parametrizacao.java
+++ back/gsw-api/src/main/java/gsw_api/gsw_api/model/Parametrizacao.java
@@ -1,17 +1,6 @@
 package gsw_api.gsw_api.model;
 
 import com.google.gson.Gson;
-import gsw_api.gsw_api.model.Noticia;
-import gsw_api.gsw_api.model.PortalNoticia;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Parametrizacao {
@@ -21,14 +10,6 @@ public class Parametrizacao {
     private String conteudo;
     private String autor;
     private String data;
-
-    // String json = "{"
-    //         + "'URL': 'Computing and Information systems',"
-    //         + "'titulo' : 'idkT',"
-    //         + "'conteudo' : 'idkC',"
-    //         + "'autor' : 'idkA',"
-    //         + "'data' : 'idkD'"
-    //         + "}";
 
     public Parametrizacao(String JSON) {
         Parametrizacao p = new Gson().fromJson(JSON, Parametrizacao.class);
@@ -78,22 +59,4 @@ public class Parametrizacao {
     public void setData(String data) {
         this.data = data;
     }
-
-    // public List<Noticia> ParametrizacaoToNoticia(PortalNoticia portal) throws IOException
-    // {
-    //     List<Noticia> noticias = new ArrayList<>();
-
-    //     Document doc = Jsoup.connect(portal.getUrl()).get();
-    //     Elements newsHeadlines = doc.select(this.URL);
-    //     for(Element headline : newsHeadlines)
-    //     {
-    //         Noticia noticia = new Noticia();
-    //         noticia.setTitulo(headline.text());
-    //         noticias.add(noticia);
-    //     }
-
-    //     return noticias;
-    // }
-
-    
 }

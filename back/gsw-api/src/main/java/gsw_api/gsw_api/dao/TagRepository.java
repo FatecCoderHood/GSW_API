f606c12e3add840f731a93613a4265f4cb25706b
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2024-11-07 17:51:22 -0300

    A3F-37 [Backend] API/Servidor: Regionalidade de tags.
    Descrição da Implementação
    
    - Agora as tags são linkadas automaticamente com outras tags existentes
    
    Sugestão de Melhoria
    
    - Melhor polimento no código
    - Permitir buscar notícias por tags, e mostrar resultado com sinônimos.

--- back/gsw-api/src/main/java/gsw_api/gsw_api/dao/TagRepository.java
+++ back/gsw-api/src/main/java/gsw_api/gsw_api/dao/TagRepository.java
@@ -3,9 +3,13 @@ package gsw_api.gsw_api.dao;
 import gsw_api.gsw_api.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long>, JpaSpecificationExecutor<Tag> {
     boolean existsByNome(String nome);
     Optional<Tag> findByNome(String nome);
+    
+    List<Tag> findByNomeIn(List<String> nomes);
 }

33e0fb7c81b5001eec40fb49490d5f9f2bfbfdc5
Alexandre Jonas <jonassouzafonseca@gmail.com>
2024-11-09 21:12:59 -0300

    A3F-45 [Backend] Notícia/API: Periodicidade Scraping.
    Descrição da Implementação
    - Rotinas que são executadas em um determinado intervalo para realizar o scraping dos portais configurados
    
    Sugestões de Melhorias Futuras
    - Validações de configuração da aplicação

--- back/gsw-api/src/main/java/gsw_api/gsw_api/GswApiApplication.java
+++ back/gsw-api/src/main/java/gsw_api/gsw_api/GswApiApplication.java
@@ -3,9 +3,11 @@ package gsw_api.gsw_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "gsw_api.gsw_api")
+@EnableScheduling
 public class GswApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GswApiApplication.class, args);
--- back/gsw-api/src/main/java/gsw_api/gsw_api/service/PortalNoticiaService.java
+++ back/gsw-api/src/main/java/gsw_api/gsw_api/service/PortalNoticiaService.java
@@ -4,6 +4,8 @@ import gsw_api.gsw_api.dao.PortalNoticiaRepository;
 import gsw_api.gsw_api.dto.DadosPortalNoticia;
 import gsw_api.gsw_api.model.PortalNoticia;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,6 +48,16 @@ public class PortalNoticiaService {
     // Método para buscar todos os portais de notícias
     public List<PortalNoticia> findAll() {
         return portalNoticiaRepository.findAll();
+    }
+
+    // Método para buscar todos os portais de notícias filtrando pela periodicidade
+    public List<PortalNoticia> findAllByPeriodicidade(String periodicidade) {
+
+        PortalNoticia portalExample = new PortalNoticia();
+        portalExample.setPeriodicidade(periodicidade);
+        Example<PortalNoticia> example = Example.of(portalExample);
+
+        return portalNoticiaRepository.findAll(example, Sort.unsorted());
     }
 
     // Método para atualizar um portal de notícia
